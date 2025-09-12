0e043cd7aeb9cabbcb26dd4aab6e32e8925b7e3f
rohitmaji004 <153757813+rohitmaji004@users.noreply.github.com>
2025-09-12 14:57:28 +0530

    Delete src directory

--- src/main/java/com/icdc/insurance/repo/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.icdc.insurance.repo;
-
-import com.icdc.insurance.model.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-}
